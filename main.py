a0dd65700cb9d201b036df9caad77d82bc343da7
Pablo del Arco <63775967+pablodelarco@users.noreply.github.com>
2023-09-04 11:43:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import xlsxwriter
 from datetime import time
-
+ 
 
 kpi_number_list = ['NR_578C', 'NR_579C', 'NR_5258A', 'NR_5259A', 'NR_5739A', 'NR_5740A']
 kpi_number_reference = ['NR_5192D', 'NR_5193B', 'NR_716A', 'NR_718A']
 
- 
+
 
 def process_worksheet(df, kpi_name):
     kpi_df = df[["Period start time", "NRCEL name", kpi_name]].sort_values(by="Period start time")
@@ -15,11 +15,10 @@ def process_worksheet(df, kpi_name):
 
     kpi_df["Time"] = pd.to_datetime(kpi_df.index).time
     kpi_df["Date"] = pd.to_datetime(kpi_df.index).date
- 
+
     return kpi_df
 
  
-
 def main():
     excel_file = "System_Program.xlsx"
     excel_file_reference = "Users.xlsx"
@@ -32,11 +31,10 @@ def main():
     excel_data = pd.ExcelFile(excel_file)
     worksheet_names = excel_data.sheet_names
 
- 
+
     excel_data_2 = pd.ExcelFile(excel_file_reference)
     worksheet_names_2 = excel_data_2.sheet_names
 
- 
 
     kpi_data = {}
     kpi_numbers = {}
@@ -50,12 +48,13 @@ def main():
             kpi_names_2 = df.columns[4:]
             kpi_numbers_2[worksheet_name] = df.iloc[0, 4:].tolist()
 
- 
 
             for kpi_name, kpi_number in zip(kpi_names_2, kpi_numbers_2[worksheet_name]):
                 if kpi_number in kpi_number_reference:
                     new_df = process_worksheet(df, kpi_name)
                     new_df.drop(columns=["Time"], inplace=True)  # Drop the "Time" column from the DataFrame
+                    new_df.drop(columns=["Date"], inplace=True) # Drop the "Date" column from the DataFrame
+                    
                     kpi_data_2[kpi_name] = (new_df, kpi_number)
 
 
@@ -70,52 +69,51 @@ def main():
                 if kpi_number in kpi_number_list:
                     kpi_df = process_worksheet(df, kpi_name)
                     kpi_data[kpi_name] = (kpi_df, kpi_number)
- 
 
-    output_file = "python_excel_August_30.xlsx"
+
+    output_file = "python_excel_August_31.xlsx"
     with pd.ExcelWriter(output_file, engine="xlsxwriter") as writer:
         workbook = writer.book
 
- 
 
         for kpi_name, (kpi_df, kpi_number) in kpi_data.items():
             worksheet = writer.book.add_worksheet(kpi_number)
-            red_background_format = workbook.add_format({"bg_color": "#FFC7CE"})
-            green_background_format = workbook.add_format({"bg_color": "#C6EFCE"})
             merge_format = workbook.add_format({
                 "bold": 1,
                 "border": 1,
                 "align": "center",
                 "valign": "vcenter",
             })
-
  
 
             worksheet.merge_range(0, 2, 0, 4, kpi_name, merge_format)
             worksheet.merge_range(1, 2, 1, 4, kpi_number, merge_format)
-
  
 
             color_index = 0
             for row_num, (index, row) in enumerate(kpi_df.iterrows(), start=3):
                 time_value = row["Time"]
                 date_value = row["Date"]
-                
+
                 if pd.notna(time_value):
                     for i in range(0, len(time_ranges)-1, 2):
                         start_time = time_ranges[i]
                         end_time = time_ranges[i+1]
-                        
+
                         start_date = kpi_df.loc[kpi_df['Time'] == start_time, 'Date'].item()
                         end_date = kpi_df.loc[kpi_df['Time'] == end_time, 'Date'].item()
+                        
+                        red_background_format = workbook.add_format({"bg_color": "#FFC7CE"})
+                        green_background_format = workbook.add_format({"bg_color": "#C6EFCE"})
+
 
                         # Compare both time and date
                         if ((start_time <= time_value <= end_time) or not((start_time > time_value > end_time)) and start_date != end_date):
-                                
+
                             if color_index == 0:
                                 worksheet.conditional_format(row_num, 2, row_num, 4, {
                                     'type':     'cell',
-                                    'criteria': '>',
+                                    'criteria': '>=',
                                     'value':    0,
                                     'format':   red_background_format
                                 })
@@ -124,17 +122,15 @@ def main():
                             else:
                                 worksheet.conditional_format(row_num, 2, row_num, 4, {
                                     'type':     'cell',
-                                    'criteria': '>',
+                                    'criteria': '>=',
                                     'value':    0,
                                     'format':   green_background_format
                                 })
                                 if time_value == end_time:
                                     color_index = 0
                             break
-    
-            
 
-
+ 
             kpi_df.drop(columns=["Time"], inplace=True)  # Drop the "Time" column from the DataFrame
             kpi_df.drop(columns=["Date"], inplace=True) # Drop the "Date" column from the DataFrame
 
@@ -144,20 +140,21 @@ def main():
             worksheet.set_column(2, len(kpi_df.columns) + 1, 15)
             worksheet.write_string(3, 1, '')
 
+ 
+            ##################### Reference data ##################### 
             idx = 2
             for kpi_name_2, (kpi_df_2, kpi_number_2) in kpi_data_2.items():
                 start_col = len(kpi_df.columns) + idx
                 kpi_df_2.to_excel(writer, sheet_name=kpi_number, startcol=start_col, startrow=2, index=False)
-                worksheet.set_column(start_col, len(kpi_df.columns) + 1, 15)
                 worksheet.write_string(3, start_col, '')
                 worksheet.merge_range(0, start_col + 1, 0, start_col + len(kpi_df.columns)-1, kpi_name_2, merge_format)
                 worksheet.merge_range(1, start_col + 1, 1, start_col + len(kpi_df.columns)-1, kpi_number_2, merge_format)
+                worksheet.set_column(start_col + 3, len(kpi_df.columns) + 1, 15)
+                
                 idx = idx + len(kpi_df_2.columns)
 
 
     print(f"Combined tables saved to '{output_file}'")
-
-
  
 
 if __name__ == "__main__":
